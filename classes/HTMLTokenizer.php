e3b6a300ab55d3791a03be8acf1d838f8b06cd78
Bob Fanger <bob.fanger@noprotocol.nl>
2011-10-06 13:35:30 +0200

    Added support for <script> tags in the HtmlTokenizer
    The body of the <script> tag becomes a T_SCRIPT token.

--- classes/HTMLTokenizer.php
+++ classes/HTMLTokenizer.php
@@ -2,58 +2,51 @@
 /**
  * Een tokenizer voor htmlcode.
  * Met name geschikt voor html met fouten.
- * Met de uitvoer kun je de exacte (foute) html weer reconstrueren. 
- * 
+ * Met de uitvoer kun je de exacte (foute) html weer reconstrueren.
+ *
  * Vaak is een oplossing mogelijk met DOMDocument of SimpleXML.
  * Gebruik in die gevallen niet deze tokenizer.
- * 
+ *
  * Gebruik deze tokenizer als je een tag/attribuut wilt vervangen en de overige (mogelijk foutieve) html intact wilt laten.
  * Daarnaast is het triviaal om een syntax highlighter te maken met deze tokenizer.
- * 
+ *
  * Tokens
  * -------------------
- * 
+ *
  * <T_TAG T_ATTRIBUTE=T_VALUE>T_TEXT</T_CLOSE_TAG>
  * ^     ^ T_WHITESPACE      ^
  * ^ T_OPEN                  ^ T_CLOSE
- * 
- * 
+ *
+ *
  * T_TAG        Het type element 'div', 'a', 'br', enz. Kan een <a> of <br /> zijn.
  * T_WHITESPACE Een spatie of newline binnen een tag.
  * T_ATTRIBUTE  De naam van het attribuut bv 'href'
  * T_VALUE      De waarde van een attribuut
- * T_TEXT      De tekst voor of na een T_TAG of T_CLOSE_TAG 
+ * T_TEXT      De tekst voor of na een T_TAG of T_CLOSE_TAG
  * T_CLOSE_TAG  Het type sluit tag bevat 'a' van een '</a>'
  * T_LT         Een '<' die niet voor opmaak gebruikt wordt
  * T_DELIMITER  De " of ' die om de T_VALUE staan
  * T_COMMENT    De inhoud van een comment blok
- * 
+ * T_SCRIPT     De inhoud van de een <script> blok
+ *
  * T_CDATA
  * T_DTD_ENTITY
  * T_INVALID    Een character dat niet klopt. Deze zal de parsen moeten/kunnen negeren.
- * 
- * @todo T_DTD_ATTRIBUTES opslitsen in meerdere T_DTD_ATTRIBUTE tokens 
- * 
+ *
+ * @todo T_DTD_ATTRIBUTES opslitsen in meerdere T_DTD_ATTRIBUTE tokens
+ *
  * Niet alle tokens hebben een type. Deze tokens bestaan dan uit de data-string i.p.v. een array(token-type, data-string)
  *
  *
- * @link http://en.wikipedia.org/wiki/HTML_element#Syntax 
- * 
- * @package Core 
+ * @link http://en.wikipedia.org/wiki/HTML_element#Syntax
+ *
+ * @package Core
  */
 namespace SledgeHammer;
 class HTMLTokenizer extends Object implements \Iterator{
 
-
-
-
-  // Readable code
 	// Genereer parser warnings (Makes UnitTests easier)
 	// Design upgradeable naar streams interface
-	// Werkt met offsets
-	// Scope:
-	// No special case for <script>:
-	// (parse <script> (and there contents) as normal tags)
 	public
 		$warnings = array();
 
@@ -62,6 +55,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 		$position,
 		$state, // CONTENT, TAG_BODY, VALUE
 		$tagType, // T_TAG of T_CLOSE_TAG
+		$currentTag,
 		$dtdLevel, // int
 		$tokenQueue = array(),
 
@@ -82,7 +76,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 		$this->wsPattern = implode($this->wsArray);
 		$this->rewind();
 	}
-	
+
 	function rewind() {
 		if ($this->iteratorKey === 0) {
 			return;
@@ -90,7 +84,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 		$this->iteratorKey = 0;
 		$this->dtdLevel = 0;
 		$this->state = 'CONTENT';
-		
+
 		$this->position = 0;
 		$this->tokenQueue = array();
 		$this->iteratorCurrent = null;
@@ -112,28 +106,32 @@ class HTMLTokenizer extends Object implements \Iterator{
 				case 'CONTENT':
 					$this->parseContent();
 					break;
-	
+
 				case 'TAG':
 					$this->parseTag();
 					break;
-	
+
 				case 'TAG_BODY':
 					$this->parseTagBody();
 					break;
-	
+
 				case 'VALUE':
 					$this->parseValue();
 					break;
-	
+
 				case 'COMMENT':
 					$this->parseHtmlComment();
 					break;
-	
+
 				case 'DTD_ELEMENTS':
 					$this->parseDTDElements();
 					break;
-					
-				default: 
+
+				case 'SCRIPT':
+					$this->parseScript();
+					break;
+
+				default:
 					throw new \Exception('Unknown state: "'.$this->state.'"');
 			}
 		} catch (\Exception $exception) {
@@ -148,10 +146,10 @@ class HTMLTokenizer extends Object implements \Iterator{
 		}
 		return array_shift($this->tokenQueue);
 	}
-	
+
 	/**
 	 * Verzameld alle T_TEXT tokens
-	 * 
+	 *
 	 */
 	function parseContent() {
 		// @todo T_GT token '>' detectie .
@@ -185,7 +183,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 			return;
 		}
 		if ($nextChar == '?') {
-			$this->parseParserTag(); 
+			$this->parseParserTag();
 			return;
 		}
 		if ($nextChar == '!') {
@@ -208,13 +206,14 @@ class HTMLTokenizer extends Object implements \Iterator{
 		if ($pos == $this->position) { // Wordt er tag vermeld aan het begin van de tag?
 			$this->warning('No tag defined');
 		} else {
+			$this->currentTag = ($this->tagType == 'T_TAG') ? $this->substr(0, $pos - $this->position) : null;
 			$this->buildToken($pos - 1, $this->tagType); // Tag is bekend
 		}
 		$this->state = 'TAG_BODY'; // We gaan over tot het parsen van de attributen
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	function parseTagBody() {
 		$this->collectWhitespace();
@@ -223,6 +222,13 @@ class HTMLTokenizer extends Object implements \Iterator{
 		if ($pos === $this->position) { // Einde tagBody
 			$this->buildToken($this->position + strlen($match) - 1, 'T_CLOSE');
 			$this->state = 'CONTENT';
+			if ($this->currentTag == 'script') {
+				if ($match == '>') {
+					$this->state = 'SCRIPT';
+				} else {
+					$this->warning('Self-closing "<script />" tags aren\'t supported by all browsers');
+				}
+			}
 			return;
 		}
 
@@ -254,10 +260,10 @@ class HTMLTokenizer extends Object implements \Iterator{
 	}
 
 	function parseValue() {
-		$invalid = ($this->tagType === 'T_CLOSE_TAG'); // T_CLOSE_TAG mogen geen attributen bevatten. 
+		$invalid = ($this->tagType === 'T_CLOSE_TAG'); // T_CLOSE_TAG mogen geen attributen bevatten.
 		$t_value = ($invalid ? 'T_INVALID': 'T_VALUE');
 		$t_delimiter = ($invalid ? 'T_INVALID': 'T_DELIMITER');
-		
+
 		$delimiter = $this->getChar();
 		if (in_array($delimiter, array('"',"'"))) { // Heeft de waarde delimiters?
 			$this->buildToken($this->position, $t_delimiter);
@@ -267,7 +273,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 				$this->buildLastToken($t_value);
 				return;
 			}
-			if ($pos !== $this->position) { // *Geen* Lege(alt="") waarde? 
+			if ($pos !== $this->position) { // *Geen* Lege(alt="") waarde?
 				$this->buildToken($pos - 1, $t_value);
 			}
 			$this->buildToken($this->position, $t_delimiter);
@@ -296,11 +302,11 @@ class HTMLTokenizer extends Object implements \Iterator{
 		} else {
 			$this->buildToken($pos - 1, 'T_COMMENT');
 		}
-		$this->buildToken($this->position + 2); 
+		$this->buildToken($this->position + 2);
 		$this->state = 'CONTENT';
 
 	}
-	 
+
 	/**
 	 * <?xml ?> en <?php ?> tags
 	 */
@@ -321,7 +327,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 	 * @link http://nl.wikipedia.org/wiki/Document_Type_Definition#Voorbeeld_2
 	 */
 	function parseDTDTag() {
-		if ($this->substr(0, 2) !== '<!') { 
+		if ($this->substr(0, 2) !== '<!') {
 			throw new \Exception('parseDTDTag() saninty check failed');
 		}
 		// @link http://en.wikipedia.org/wiki/CDATA#CDATA_sections_in_XML
@@ -329,7 +335,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 			$this->buildToken($this->position + 8);
 			$pos = $this->strpos(']]>');
 			if ($pos === false) {
-				$this->warning('CDATA not terminated'); 
+				$this->warning('CDATA not terminated');
 				$this->buildLastToken('T_CDATA');
 				return;
 			}
@@ -345,7 +351,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 		$this->buildToken($this->position + 1);
 		$pos = $this->firstOccurrance(array_merge(array('[', '>'), $this->wsArray));
 		if ($pos === false) {
-			$this->warning('Inline DTD tag not terminated'); 
+			$this->warning('Inline DTD tag not terminated');
 			$this->buildLastToken('T_DTD_ENTITY');
 			return;
 		}
@@ -360,7 +366,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 	function parseDTDAttributes($state) {
 		$pos = $this->firstOccurrance(array('[', '>'), $match);
 		if ($pos === false) {
-			$this->warning('Inline DTD tag not terminated'); 
+			$this->warning('Inline DTD tag not terminated');
 			$this->buildLastToken('T_DTD_ATTRIBUTES');
 			return;
 		}
@@ -386,12 +392,12 @@ class HTMLTokenizer extends Object implements \Iterator{
 		$this->collectWhitespace();
 		$pos = $this->firstOccurrance(array('<!', ']', '>'), $match);
 		if ($pos === false) {
-			$this->warning('Invalid DTD elements'); 
+			$this->warning('Invalid DTD elements');
 			$this->buildLastToken('T_DTD_ATTRIBUTES');
 			return;
 		}
 		if ($match == '>') {
-			$this->warning('Invalid DTD elements'); 
+			$this->warning('Invalid DTD elements');
 			$this->dtdLevel = 0;
 			if ($this->position != $pos) {
 				$this->buildToken($pos - 1, 'T_INVALID');
@@ -410,6 +416,21 @@ class HTMLTokenizer extends Object implements \Iterator{
 			$this->buildToken($pos - 1, 'T_INVALID');
 		}
 		$this->parseDTDTag();
+	}
+
+	function parseScript() {
+		$pos = $this->stripos('</script');
+		if ($pos === false) {
+			$this->warning('Unterminated <script> tag');
+			$this->buildLastToken('T_SCRIPT');
+			return;
+		}
+		$this->state = 'TAG';
+		if ($this->position == $pos) { // No script body?
+			$this->parseTag();
+			return;
+		}
+		$this->buildToken($pos - 1, 'T_SCRIPT');
 	}
 
 	function collectWhitespace($tokenType = 'T_WHITESPACE') {
@@ -431,7 +452,7 @@ class HTMLTokenizer extends Object implements \Iterator{
 
 	/**
 	 * Een token instellen o.b.v. van de eind positie van de token.
-	 * Zal de $this->position bijwerken 
+	 * Zal de $this->position bijwerken
 	 */
 	function buildToken($endPosition, $tokenType = null) {
 		$length = $endPosition + 1 - $this->position;
@@ -489,6 +510,14 @@ class HTMLTokenizer extends Object implements \Iterator{
 	}
 
 	/**
+	 * Een strpos op de $html
+	 * relatief vanaf de huidige $position
+	 */
+	function stripos($needle) {
+		return stripos($this->html, $needle, $this->position);
+	}
+
+	/**
 	 * @return int de positie van de needle die het eerste voorkomt in de string
 	 */
 	function firstOccurrance($needles, &$match = null) {
@@ -510,9 +539,9 @@ class HTMLTokenizer extends Object implements \Iterator{
 	}
 
 	function warning($message) {
-		$this->warnings[] = $message.' on position '.$this->position.' parsing '.$this->state; 
+		$this->warnings[] = $message.' on position '.$this->position.' parsing '.$this->state;
 	}
-	
+
 	function key() {
 		return $this->iteratorKey;
 	}
@@ -538,10 +567,10 @@ class HTMLTokenizer extends Object implements \Iterator{
 		}
 		$this->iteratorCurrent = $this->parseToken();
 	}
-	
+
 	/**
-	 * Geef een syntax highlighted versie van de html. (Het title attribute wordt ingesteld met het tag type)  
-	 * 
+	 * Geef een syntax highlighted versie van de html. (Het title attribute wordt ingesteld met het tag type)
+	 *
 	 * @param $charset
 	 * @return void
 	 */
@@ -552,33 +581,33 @@ class HTMLTokenizer extends Object implements \Iterator{
 			'T_CLOSE_TAG' => 'purple',
 			'T_OPEN' => 'green',
 			'T_CLOSE' => 'green',
-		
+
 			'T_ATTRIBUTE' => 'brown',
 			'T_VALUE' => 'darkblue',
 			'T_COMMENT' => 'gray',
 			'T_DTD_ENTITY' => 'orange',
 			'T_DTD_ATTRIBUTES' => 'brown',
-		
+
 			'T_TEXT' => 'black',
 			'T_CDATA' => 'black',
 			'T_WHITESPACE' => 'red',
 			'T_INVALID' => $errorColor,
 			'T_LT' => $errorColor,
 			'T_GT' => $errorColor,
-		
+
 			'T_PARSER_TAG' => 'Aquamarine',
-		
+
 			'T_DELIMITER' => 'orange',
 		);
 		echo '<pre style="background:white;overflow:auto;padding:10px;color:red">';
 		foreach ($this as $index => $token) {
 			if (is_array($token)) {
-				echo '<span title="', $token[0] ,'" style="color:' , $colors[$token[0]], '">', htmlentities($token[1], ENT_COMPAT, $charset), '</span>';				
+				echo '<span title="', $token[0] ,'" style="color:' , $colors[$token[0]], '">', htmlentities($token[1], ENT_COMPAT, $charset), '</span>';
 			} else {
 				echo '<span style="color:green">', htmlentities($token, ENT_COMPAT, $charset), '</span>';
 			}
 		}
 		echo "</pre>";
-	}	
+	}
 }
 ?>
