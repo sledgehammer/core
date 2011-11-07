e44982bfdb7e28ecc56f18fd0d58728432dc7022
Bob Fanger <bob.fanger@noprotocol.nl>
2011-11-07 12:31:39 +0100

    Documented Text methods

--- classes/Text.php
+++ classes/Text.php
@@ -3,7 +3,7 @@
  * Text, a string class for handeling (multibyte) strings with OOP syntax
  * Modelled after the C# String class.
  * @link http://msdn.microsoft.com/en-us/library/system.string.aspx
- * 
+ *
  * @package Core
  */
 namespace SledgeHammer;
@@ -13,9 +13,9 @@ class Text extends Object implements \ArrayAccess {
 	 * @var int  The number of characters
 	 */
 	public $length;
-	
+
 	/**
-	 * @var string  The string in UTF-8 
+	 * @var string  The string in UTF-8
 	 */
 	private $text;
 
@@ -42,11 +42,11 @@ class Text extends Object implements \ArrayAccess {
 	function __toString() {
 		return $this->text;
 	}
-	
+
 	/**
 	 * Virtual propeties like "length"
 	 * @param string $property
-	 * @return mixed 
+	 * @return mixed
 	 */
 	function __get($property) {
 		if ($property == 'length') {
@@ -54,26 +54,65 @@ class Text extends Object implements \ArrayAccess {
 		}
 		parent::__get($property);
 	}
-	
+
 	// Mutations
-	
+
+	/**
+	 * Returns a copy of this text converted to uppercase.
+	 *
+	 * @return Text
+	 */
 	function toUpper() {
 		return new Text(mb_strtoupper($this->text, 'UTF-8'), 'UTF-8');
 	}
-	
+	/**
+	 * Returns a copy of this text converted to lowercase.
+	 *
+	 * @return Text
+	 */
 	function toLower() {
 		return new Text(mb_strtolower($this->text, 'UTF-8'), 'UTF-8');
 	}
+	/**
+	 * Removes all leading and trailing white-space characters from the current text.
+	 * @link http://php.net/manual/en/function.trim.php
+	 *
+	 * @param $charlist  (optional) The stripped characters can also be specified. list the characters that you want to be stripped.
+	 * @return Text
+	 */
 	function trim($charlist = null) {
 		return new Text(trim($this->text, $charlist), 'UTF-8');
 	}
+
+	/**
+	 * Removes all leading white-space characters from the current text.
+	 * @link http://php.net/manual/en/function.ltrim.php
+	 *
+	 * @param $charlist  (optional) The stripped characters can also be specified. list the characters that you want to be stripped.
+	 * @return Text
+	 */
 	function trimStart($charlist = null) {
 		return new Text(ltrim($this->text, $charlist), 'UTF-8');
 	}
+	/**
+	 * Removes all trailing occurrences white-space characters from the current text.
+	 * @link http://php.net/manual/en/function.rtrim.php
+	 *
+	 * @param $charlist  (optional) The stripped characters can also be specified. list the characters that you want to be stripped.
+	 * @return Text
+	 */
 	function trimEnd($charlist = null) {
 		return new Text(rtrim($this->text, $charlist), 'UTF-8');
 	}
-	
+
+	/**
+	 * Returns a substring from this text.
+	 * Similar to substr()
+	 *
+	 * @param int $offset
+	 * @param int $length  (optional)
+	 * @return Text
+	 */
 	function substring($offset, $length = null) {
 		if ($length === null) {
 			if (mb_internal_encoding() == 'UTF-8') {
@@ -88,27 +127,47 @@ class Text extends Object implements \ArrayAccess {
 		return new Text(mb_substr($this->text, $offset, $length, 'UTF-8'), 'UTF-8');
 	}
 
+	/**
+	 * Returns a copy of this text written backwards.
+	 *
+	 * @return Text
+	 */
 	function reverse() {
 		$result = '';
 		for($i = $this->length - 1; $i >= 0; $i--) {
 			$result .= mb_substr($this->text, $i, 1, 'UTF-8');
 		}
-		return new Text($result, 'UTF-8'); 
+		return new Text($result, 'UTF-8');
 	}
 
+	/**
+	 * Returns a copy of this text in which all occurrences of $search  are replaced with $replace.
+	 *
+	 * @param string $search
+	 * @param string $replace
+	 * @return Text
+	 */
 	function replace($search, $replace) {
 		return new Text(str_replace($search, $replace, $this->text), 'UTF-8');
 	}
-	
+
+	/**
+	 * Split a string by the $separator
+	 * Similar to explode()
+	 *
+	 * @param string $separator
+	 * @param int|null $limit  (optional)
+	 * @return array
+	 */
 	function split($separator, $limit = null) {
 		$strings = explode($separator, $this->text, $limit);
 		$texts = array();
 		foreach ($strings as $text) {
 			$texts[] = new Text($text, 'UTF-8');
 		}
-		return $texts;	
+		return $texts;
 	}
-	
+
 	function ucfirst() {
 		return new Text($this[0]->toUpper().$this->substring(1), 'UTF-8');
 	}
@@ -116,12 +175,15 @@ class Text extends Object implements \ArrayAccess {
 		return new Text($this[0]->toUpper().$this->substring(1)->toLower(), 'UTF-8');
 	}
 
-	// Info 
-	
+	// Info
+
 	/**
-	 * A.k.a strpos
-	 * @param type $search
-	 * @param type $offset 
+	 * Returns the index of the first occurrence of the specified $text
+	 * Similar to strpos()
+	 *
+	 * @param string $search
+	 * @param int $offset
+	 * @return int|false
 	 */
 	function indexOf($text, $offset = 0, $ignoreCase = false) {
 		if ($ignoreCase) {
@@ -131,6 +193,12 @@ class Text extends Object implements \ArrayAccess {
 		}
 	}
 
+	/**
+	 * Determines whether the beginning of this text matches the specified $text.
+	 *
+	 * @param string $text
+	 * @return bool
+	 */
 	function startsWith($text) {
 		$text = new Text($text);
 		if ($this->substring(0, $text->length) == $text) {
@@ -138,7 +206,13 @@ class Text extends Object implements \ArrayAccess {
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Determines whether the end of this text matches the specified $text.
+	 *
+	 * @param string $text
+	 * @return bool
+	 */
 	function endsWith($text) {
 		$text = new Text($text);
 		if ($this->substring(0 - $text->length) == $text) {
@@ -146,7 +220,14 @@ class Text extends Object implements \ArrayAccess {
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Check if this text has same value as $text.
+	 *
+	 * @param Text $text
+	 * @param bool $ignoreCase
+	 * @return bool
+	 */
 	function equals($text, $ignoreCase = false) {
 		$text = new Text($text);
 		if ($ignoreCase) {
@@ -156,9 +237,9 @@ class Text extends Object implements \ArrayAccess {
 		}
 	}
 
-		//
-	
+
 	// ArrayAccess
+
 	public function offsetExists($offset) {
 		return ($offset < mb_strlen($this->text, 'UTF-8'));
 	}
@@ -170,6 +251,6 @@ class Text extends Object implements \ArrayAccess {
 	}
 	public function offsetUnset($offset) {
 		throw new \Exception('Not implemented');
-	}	
+	}
 }
 ?>
