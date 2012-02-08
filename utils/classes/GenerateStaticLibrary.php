82b7ec88d68aefb31e494bf106cf97a5a3252d37
Bob Fanger <bfanger@gmail.com>
2012-02-08 15:43:24 +0100

    Using the new static MessageBox functions

--- utils/classes/GenerateStaticLibrary.php
+++ utils/classes/GenerateStaticLibrary.php
@@ -15,11 +15,11 @@ class GenerateStaticLibrary extends Util {
 		$warningMessage = 'This will generate a <b>AutoLoader.db.php</b> file which contains the file-location for al detected classes and interfaces.<br />';
 		$warningMessage .= 'Changes in the classes folders will no longer be detected by the AutoLoader!<br />';
 		$warningMessage .= 'You\'ll need to rerun this script after those changes.';
-		$dialog = new DialogBox('warning', 'Optimize AutoLoader', $warningMessage, array('continue' => array('icon' => WEBROOT.'icons/accept.png', 'label' => 'Continue')));
+		$dialog = new DialogBox('Optimize AutoLoader', $warningMessage, array('continue' => array('icon' => WEBROOT.'icons/accept.png', 'label' => 'Continue')));
 		$answer = $dialog->import($error);
 		if ($answer == 'continue') {
 			if (!$this->isWritable()) {
-				return new MessageBox('error', 'Generating failed', 'Make sure the webuser is allowed to write "'.$this->paths['project'].'AutoLoader.db.php"');
+				return MessageBox::error('Generating failed', 'Make sure the webuser is allowed to write "'.$this->paths['project'].'AutoLoader.db.php"');
 			}
 			$util = new UtilScript('generate_static_library.php', 'Generate AutoLoader database');
 			return $util->generateContent();
